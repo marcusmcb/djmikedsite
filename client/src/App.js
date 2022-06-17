ca65b876b413356d14fda5e3e0dfc9fdfe30dfeb
Marcus McBride <marcusmcb@gmail.com>
2022-06-17 12:51:20 -0700

    added gallery & media player components

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import MainPage from './components/MainPage'
+import GalleryPage from './components/GalleryPage'
 
 import './App.css'
 
 const App = () => {
   const routes = (
     <Routes>
-      <Route path='/' exact element={<MainPage />}>        
-      </Route>
+      <Route path='/' exact element={<MainPage />}></Route>
+      <Route path='/gallery' element={<GalleryPage />}></Route>
     </Routes>
   )
 
---
+++ client/src/components/GalleryPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './gallerypage.css'
+
+const GalleryPage = () => {
+  return <p>future djmiked gallery page</p>
+}
+
+export default GalleryPage
