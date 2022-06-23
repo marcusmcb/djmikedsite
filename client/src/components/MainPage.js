e47dedf7527c092ba543f004d3b9a6361b68cb95
Marcus McBride <marcusmcb@gmail.com>
2022-06-23 15:07:59 -0700

    added main page layout and basic css

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './mainpage.css'
 
 const MainPage = () => {
-  return <p>future djmiked site</p>
+  return (
+    <Fragment>
+      <div className='navbar'>
+        <h3>future djmiked navbar</h3>
+      </div>
+      <div className='mainpage-body'>
+        <h3>main page body</h3>
+      </div>
+      <div className='media-player'>
+        <h3>media player</h3>
+      </div>
+      <div className='footer'>
+        <h3>site footer</h3>
+      </div>
+    </Fragment>
+  )
 }
 
 export default MainPage
